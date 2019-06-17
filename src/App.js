30434c0114c322d3c4c72e96a128cb7d609b443b
Justin Frey <justinfrey64@gmail.com>
2019-06-17 12:09:42 -0400

    chapter 20 complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { connect } from "react-redux";
+import Counter from "./Counter";
+ 
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    countValue: state.count
+    };
 }
+ 
+// Action
+var increaseAction = { type: "increase" };
+var decreaseAction = { type: "decrease" };
+ 
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function() {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function() {
+      return dispatch(decreaseAction);
+    }
+  };
+}
+ 
+// The HOC
+var connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
 
-export default App;
+export default connectedComponent;
